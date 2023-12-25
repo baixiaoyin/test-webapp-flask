3e3c3b6fba8ba0fce903f0eef0397709fe08a867
Xiaoyin Bai <Xiaoyin.Bai@ibm.com>
2023-12-25 17:00:57 +0800

    v1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! This is version updated with webhook! v0"
+    return "Welcome! This is version updated with webhook! v1"
 
 @app.route('/how are you')
 def hello():
